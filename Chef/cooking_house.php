ce70bc77d066db8f37d1ba83361c3d329cff1dab
morgench <48621022+morgench@users.noreply.github.com>
2019-04-08 10:12:08 +0800

    Add files via upload

--- Chef/cooking_house.php
+++ Chef/cooking_house.php
@@ -1,29 +1,22 @@
 <?php 
 require __DIR__.'/connect.php';
 $page_name = 'cooking_house';
-
 header("Content-Type:text/html; charset=utf-8");
-
 // $per_page = 10;
 // $page = isset($_GET['page'])? intval($_GET['page']):1;
-
 $p_sql = "SELECT * FROM `cooking_house` LEFT JOIN `cooking_house_photo` ON `cooking_house`.`sid`=`cooking_house_photo`.`cooking_house_sid`";
 $p_stmt = $pdo->query($p_sql);
 $all_pics = $p_stmt->fetchAll(PDO::FETCH_ASSOC);
-
 //算總比數
 $t_sql = "SELECT COUNT(1) FROM cooking_house";
 $t_stmt = $pdo->query($t_sql);
 $total_rows = $t_stmt->fetch(PDO::FETCH_NUM)[0];
-
-
 //總頁數
 // $total_pages = ceil($total_rows/$per_page);
 // if($page>$total_pages) $page =$total_pages;
 // if($page<1) $page =1;
 $sql = sprintf("SELECT * FROM cooking_house ORDER BY sid DESC LIMIT %s, %s", 0, $total_rows);
 $stmt = $pdo->query($sql);
-
 //所有資料一次拿出來
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -51,34 +44,28 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <div class ="col-md-4 center_div">
 <!-- <nav aria-label="...">
   <ul class="pagination pagination-sm">
-
   <li class="page-item">
       <a class="page-link" href="?page=1" aria-label="Previous">
         <span aria-hidden="true">&laquo;</span></a>
     </li>
-
   <li class="page-item <?= $page<=1 ?'disable':''?>"> 
         <a class ="page-link" href="?page=<?= $page-1 ?>">&lt;</a> 
   </li>
-
   <?php for($i=1;$i<=$total_pages;$i++): ?>
     <li class="page-item <?= $i==$page ? 'active': ''?>"> 
         <a class ="page-link bg-warning border-warning"  href="?page=<?= $i ?>"><?= $i ?></a> 
       </span>
     </li>
     <?php endfor ?>
-
     <li class="page-item <?= $page>=$total_pages ?'disable':''?>"> 
     <a class ="page-link" href="?page=<?= $page+1 ?>">&gt;</a> 
   </li>
-
   <li class="page-item">
       <a class="page-link" href="?page=<?= $total_pages ?>" aria-label="Next">
         <span aria-hidden="true">&raquo;</span>
         <span class="sr-only">Next</span>
       </a>
     </li>
-
   </ul>
 </nav> -->
 <!--  -->
@@ -95,7 +82,6 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </div>
 <br>
 <!-- Search 
-
 <form name="form1" action="cooking_house_data_search.php" method="post">
   <div class="form-group "style="border:1px solid skyorange">
     <label for="search_input" >&nbsp搜尋:</label>
@@ -107,7 +93,6 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   
   </div>
 <form>
-
  --> 
 
  <div class="col-lg-12 table-responsive card-list-table">
@@ -127,10 +112,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
             >
             <thead class="bg-warning text-nowrap">
     <tr>
-    <td>
-      <input type="checkbox" name="myTextEditBox" value="checked" 
-      style="margin-left:auto; margin-right:auto;">
- </td>
+    
       <th scope="col" data-sortable="true" data-align="center">#</th>
       <th scope="col"  data-sortable="true">Name</th>
       <th scope="col"  data-sortable="true">Phone</th>
@@ -148,10 +130,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <tbody>
   <?php foreach($rows as $row):?>
     <tr class="form_data_font_style">
-    <td>
-      <input type="checkbox" name="myTextEditBox" value="checked" 
-      style="margin-left:auto; margin-right:auto;">
- </td>
+ 
       <td><?=$row['sid']?></td>
       <td><?=$row['name']?></td>
       <td><?=$row['phone']?></td>
